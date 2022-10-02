5bc6cffd9f6235c1243a3f2887dd31db0c66d054
Joseph Gross <lobster@cozycampers.xyz>
2022-10-02 16:08:24 -0400

    Added GuildMembers intent for later usage

--- src/bot.js
+++ src/bot.js
@@ -8,6 +8,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
   ],
 });
 
